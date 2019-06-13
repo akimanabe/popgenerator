feee2bf7a42af61bde71fcf584440a6200e4b5b8
Akihiro Manabe <amanabe@affrc.go.jp>
2019-06-13 16:15:53 +0900

    Edit age, agemethods

--- R/agemethods.R
+++ R/agemethods.R
@@ -21,25 +21,3 @@ age_unif <- function(n,min,max){
   runif(n,min,max)
 
 }
-
-
-age <- function(n,mean,max,min,lamda,sd,method=c("Normal","Lognormal","Poisson","Uniform"),roundage=FALSE){
-
-  if(is.na(method)==TRUE){method=="Normal"}
-
-  if(method=="Normal"){
-    age_norm(n,mean,sd)
-  }
-
-  if(method=="Lognormal"){
-    age_lnorm(n,mean,sd)
-  }
-
-  if(method=="Poisson"){
-    age_pois(n,lambda)
-  }
-
-  if(method=="Uniform"){
-    age_unif(n,min,max)
-  }
-}
