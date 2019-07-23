9fda5dd2fdd2c0afab50b2cbff267a3ff2ea2e62
Akihiro Manabe <amanabe@affrc.go.jp>
2019-07-23 14:12:19 +0900

    Fix conflicts

--- R/generate_pop.R
+++ R/generate_pop.R
@@ -15,41 +15,9 @@
 #' @references Manabe, A., et al. (2018). A novel ontogenetic function incorporating energy allocation. PLoS ONE. e######.
 #'
 #' @examples
-<<<<<<< HEAD
-#' library(ggplot2)
-#' imac <- generate_pop(1000, type="mackerel")
-#' g <- ggplot(imac,aex(x=tt,t=len))
-#' graph <- g +
-#'          geom_point()+
-#'          xlab("Age (years))+
-#'          ylab("Body length (mm))
-=======
-#'
-#' generate_pop(1000,c(524,  0.19,  -1.61, 4, 2),TRUE)
-#'
->>>>>>> feat_fishtype
 
 
-
-
-
-<<<<<<< HEAD
-generate_pop <- function(n,par=NULL, type="sardine",roundage=FALSE){
-
-  # nから年齢×年齢別体長へ割り振り
-  # 基本的には年齢と年齢別体長は同じくらいの量
-  nage <- ceiling(sqrt(n))
-  lage <- ceiling(n/nage)
-
-  # Totalが足りなくなると困るからCeilingで余分にGenerateして、最後にTotnum関数でn分無作為抽出
-  # totnum <- sample(nage*lage,n,replace=FALSE,prob=NULL) #adjust to n num
-
-  # temporary parameters - for imaginary sardine "iwashi sample"
-
-=======
 generate_pop <- function(n,p,roundage=FALSE){
->>>>>>> feat_fishtype
-
 
   nage <- ceiling(sqrt(n))
   nlen <- ceiling(n/nage)
@@ -85,20 +53,6 @@ generate_pop <- function(n,p,roundage=FALSE){
 }
 
 
-<<<<<<< HEAD
-# #data with limited proportion of "aged" population
-# set.seed(10)
-# popdata <- generate_pop(1000,type="flounder",roundage=TRUE)
-#
-# aged <- dplyr::sample_n(popdata,200)
-# disaged <- dplyr::sample_n(popdata,800)
-#
-# aged <- data.frame(tt = aged$tt,len = round(aged$len))
-# disaged <- data.frame(tt = rep(NA,length(disaged$tt)),len=round(disaged$len))
-#
-=======
-
-
 
 #' Title flounder data
 #'
@@ -113,34 +67,10 @@ generate_pop <- function(n,p,roundage=FALSE){
 #'
 #' flounder(1000)
 #'
- flounder <- function(n,seed=41,roundage=FALSE){
+
+flounder <- function(n,seed=41,roundage=FALSE){
    set.seed(seed)
    pars <- c(358,  0.357, -0.15, 4, 2)
   floundata <- generate_pop(n,pars,roundage)
   return(floundata)
  }
-#
-# flounder2 <- function(n,seed,roundage=FALSE){
-#   set.seed(seed)
-#   pars <- c(358,  0.357, -0.15, 4, 2)
-#   floundata <- generate_pop(n,pars,roundage)
-  #   ggplot2::ggplot(floundata)+
-  #     geom_point(aes(tt,len))+
-  #     xlim(0,10)+
-  #     ylim(0,500)
-  #
-# }
-#
-# par(mfcol=c(4,4))
-# par(mgp=c(3,1,0))
-# par(mar=c(3,2,0,0))
-# for(i in 1:16){
-#
-# seeds <- c(33:48)
-# res <- flounder2(1000,seeds[i],roundage=TRUE)
-# plot(res, xlim=c(0,10),ylim=c(0,500))
-#
-# }
-
-#flounderseed =41 gooooood!
->>>>>>> feat_fishtype
